6bf782fae8f2673658d0a81942a3542e61daa00b
Isabel Costa <isabelcmdcosta@gmail.com>
2017-06-24 16:09:43 +0100

    created organization controller

---
+++ app/controllers/organizations_controller.rb
@@ -0,0 +1,33 @@
+class OrganizationsController < ApplicationController
+
+  def home
+  end
+
+  def index
+    @organizations = Organization.all()
+  end
+
+  def new
+    @organization = Organization.new()
+  end
+
+  def edit
+    set_organization
+  end
+
+  def show
+    set_organization
+    session[:organization_id] = @organization.id
+  end
+
+  private
+
+  def organization_params
+    params.require(:organization).permit(:title, :description, :interests, :skills, :avatar)
+  end
+
+  def set_organization
+    @organization = Organization.find(params[:id])
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
+
+  resources :organizations
+  resources :refugees
 end
