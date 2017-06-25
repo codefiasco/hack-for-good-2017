7256e96d0dd9979e0a2c09b22adce6503cfff8cf
Francisco Machado <franm912@gmail.com>
2017-06-25 07:48:59 +0100

    Refugees and Jobs Relations table and model

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,8 @@ class Job < ActiveRecord::Base
   has_many :jobs_skills_links
   has_many :skills, through: :jobs_skills_links
 
+  has_many :refugees_jobs_links
+  has_many :refugees, through: :refugees_jobs_links
+
   belongs_to :organization
 end
