841d9f0245f46b83820feebe2925b8b0fc2fe6be
Francisco Machado <franm912@gmail.com>
2017-06-25 03:33:00 +0100

    Job create action

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -7,4 +7,26 @@ class JobsController < ApplicationController
   def show
     @job = Job.find(params[:id])
   end
+
+  def create
+    skills = params[:job][:skills]
+    @job = Job.new(title: params[:job][:title], description: params[:job][:description], expiration_date: params[:job][:expiration_date], location: params[:job][:location])
+    @job.organization_id = session[:org_id] 
+
+    if @job.save
+      skills.each do |s|
+        rel = JobsSkillsLink.new(job_id: @job.id, skill_id: s.to_i)
+        rel.save
+      end
+
+      redirect_to jobs_path
+    else
+
+    end
+
+  end
+
+  private
+  def jobs_params
+  end
 end
