a16140acc983d29b704201a2f14e9f28d4e419a3
Francisco Machado <franm912@gmail.com>
2017-06-25 04:44:08 +0100

    Refugee model skills link

--- app/models/refugee.rb
+++ app/models/refugee.rb
@@ -3,7 +3,10 @@ class Refugee < ActiveRecord::Base
   has_many :professions, through: :refugees_professions_links
 
   has_many :refugees_languages_links
-  has_many :languages, through: :refugees_languages_links
+  has_many :languages, through: :refugees_languages_links4
+
+  has_many :skills_refugees_links
+  has_many :skills, through: :skills_refugees_links
 
   has_secure_password
 
